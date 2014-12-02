419546a3dfdb1a9327063881d8731db86ffcee51
Thomas 'PointedEars' Lahn <PointedEars@web.de>
2014-12-02 17:09:17 +0100

    git-delete-tag.sh: Pull tags before deleting one
    Signed-off-by: Thomas Lahn <thomas.lahn@scout24.ch>

--- git-delete-tag.sh
+++ git-delete-tag.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-git tag -d "${@:?"Usage: ${0##*/} TAG..."}" &&
-  git push origin$(printf ' :refs/tags/%s' "$@") &&
-  git push --tags
+git pull --tags &&
+	git tag -d "${@:?"Usage: ${0##*/} TAG..."}" &&
+	git push origin$(printf ' :refs/tags/%s' "$@") &&
+	git push --tags
