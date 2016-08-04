b4410d83d85542ff1c7d634c01aba5d50a73af7d
Thomas 'PointedEars' Lahn <PointedEars@web.de>
2016-08-04 11:04:24 +0200

    Added git-root

---
+++ git-root.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+## git-root -- outputs the current Git repository's root directory
+## Copyright (c) 2016  Thomas 'PointedEars' Lahn <PointedEars@web.de>.
+## Distributed under the GNU General Public License, version 3 (GPLv3).
+##
+## Make it executable, symlink it as `git-root' in a directory in your PATH,
+## and execute
+##
+## export PROMPT_COMMAND="$PROMPT_COMMAND;"' [ -n "$(which git-root)" ] && export GIT_ROOT=$(git root --relative 2>/dev/null)'
+##
+## when initialising the shell (e.g. in .bashrc) for a GIT_ROOT
+## environment variable that always has the current git root directory relative path
+## as its value.  You can then write, e.g.,
+##
+## cd $GIT_ROOT
+##
+## to change to the Git root directory, observing symlinks (without `--relative',
+## the absolute path is output).
+
+if git rev-parse >/dev/null 2>&1; then
+  if [ "$1" == '--relative' ]; then
+    git rev-parse --show-cdup
+  else
+    git rev-parse --show-toplevel
+  fi
+fi
