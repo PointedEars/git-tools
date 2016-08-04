30216cc725233f2cf3a3bfd4eb964dcf27c1b4e5
Thomas 'PointedEars' Lahn <PointedEars@web.de>
2016-08-04 11:03:44 +0200

    Added git-move-tag

---
+++ git-move-tag.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ -z "$1" ] || [ -z "$2" ]; then
+  echo >&2 "Usage: ${0##*/} TAG TARGET"
+  exit 1
+fi
+
+git pull --tags &&
+  git tag -a "$1" "$2" -f &&
+  git push origin --tags -f
