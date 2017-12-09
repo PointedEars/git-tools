d6d2a22b31e6f13ef24bfd2a02b2387293f97b13
Thomas 'PointedEars' Lahn <PointedEars@web.de>
2017-12-09 23:39:48 +0100

    git-search-branch: Added help and text formatting

--- git-search-branch.sh
+++ git-search-branch.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-re=${1:?"Usage: ${0##*/} [ERE] [egrep-options]"}
+. text-formatting 2>/dev/null
+
+if [ -z "$1" ]; then
+  echo >&2 "Searches all git branches for a file.
+
+Usage: $text_bold${0##*/}$text_norm \
+[${text_ital}ERE$text_norm] [${text_ital}egrep-options$text_norm]"
+  exit 1
+fi
+
+re=$1
 shift
 for branch in $(git for-each-ref --format="%(refname:strip=2)" refs/heads)
 do
-  printf '\n%s:\n\n' "$branch"
+  printf '\n%s%s:%s\n' "$text_bold" "$branch" "$text_norm"
   git ls-tree -r --name-only "$branch" | egrep -e "$re" "$@"
 done
